c77ed2261c518dd3d5689bb60556699cffc7f9f5
Anim <Anim-101@users.noreply.github.com>
2019-01-06 17:53:01 +0600

    Update ActionG.java

--- GameStoreM/GameStoreManagement/GameStoreM/Frames/SubFrames/ActionG.java
+++ GameStoreM/GameStoreManagement/GameStoreM/Frames/SubFrames/ActionG.java
@@ -6,6 +6,7 @@ import GameStoreM.Modifiers.*;
 import GameStoreM.Frames.SubFrames.*;
 import GameStoreM.Frames.BridgeFrames.*;
 //Packaging Ends Here
+
 import java.awt.*;
 import javax.swing.*;
 import java.awt.event.*;
@@ -21,10 +22,9 @@ public final class ActionG extends JFrame
 		//Fow Viewing Available games;
 		Vector data = new Vector ();
 		try
-		
 		{	
 			Class.forName("com.mysql.jdbc.Driver");
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/GameStoreM", "root", "");
+           		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/GameStoreM", "root", "");
 			Statement stmt = con.createStatement();
 			String query = "SELECT * FROM ACTION";
 			ResultSet rs = stmt.executeQuery(query);
@@ -38,35 +38,33 @@ public final class ActionG extends JFrame
 				data.add(row);
 			}
 		}
-		
 		catch (Exception ex)
-			{
-				JOptionPane.showMessageDialog(this,"No Games Found");
-			}
-			
-			Vector col = new Vector ();
-			col.add("Action");
-			col.add("Copy");
-			col.add("Price per Copy");
-			actionTable = new JTable(data,col);
-			JScrollPane pnl = new JScrollPane (actionTable);
-			
-			this.getContentPane().add(pnl);
-			this.setLayout(new BoxLayout(this.getContentPane(),BoxLayout.Y_AXIS));
-			this.setSize(400,400);
-			this.setVisible(true);
+		{
+			JOptionPane.showMessageDialog(this,"No Games Found");
+		}			
+		Vector col = new Vector ();
+		col.add("Action");
+		col.add("Copy");
+		col.add("Price per Copy");
+		actionTable = new JTable(data,col);
+		JScrollPane pnl = new JScrollPane (actionTable);
+
+		this.getContentPane().add(pnl);
+		this.setLayout(new BoxLayout(this.getContentPane(),BoxLayout.Y_AXIS));
+		this.setSize(400,400);
+		this.setVisible(true);
 			
 		addWindowListener(new WindowAdapter()
+		{
+			public void windowClosing(WindowEvent e) 
 			{
-				public void windowClosing(WindowEvent e) 
-				{
-					setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-					Gener lol = new Gener ();
-					lol.setVisible(true);
-				}
-			});
-			
+				setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+				Gener lol = new Gener ();
+				lol.setVisible(true);
+			}
+		});	
 	}
+	
 	public static void main (String [] args)
 	{
 	}
